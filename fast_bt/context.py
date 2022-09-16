23917c1d58d1d6ed7a6b36394b97b7a76c6f3783
RuitaoWang <wang.ruitao@outlook.com>
2022-09-16 23:33:27 +0800

    constant variable

---
+++ fast_bt/context.py
@@ -0,0 +1,36 @@
+"""
+Constant variable
+"""
+
+
+class Context:
+    """
+    The Global variable to keep track of current market quote
+
+    Atttribute
+    ----------
+    cur_time: pd.Timestamp
+        current time
+    pre_time: pd.Timestamp
+        time one step ago
+    cur_prc: dict
+        current market quote, each key should be a field (price, volume, etc) in pd.Series
+    pre_prc: dict
+        previous market quote
+    """
+    cur_time = None
+    pre_time = None
+    cur_quote = {}
+    pre_quote = {}
+
+
+class TradingParam:
+    """
+    回测中的交易常量
+    """
+    # sllipage
+    sllipage = 0.002
+    # commission
+    commission = 0.001
+    # max percentage of the snapshot's amount that are allowed 
+    max_trading_percentage = 0.05
